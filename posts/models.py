fa3f9ae82f67d7f913884197bfc8dda58bb80c4f
puntusap <puntus99_99@mail.ru>
2020-01-17 18:22:44 +0700

    model post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Network(models.Model):
+    name=models.CharField(max_length=255)
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    image=models.CharField(max_length=255)
+    time=models.DateTimeField()
+    socnet=models.ForeignKey('Network', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
